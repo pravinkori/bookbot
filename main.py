06f13817f83e1727a1ed26344d58b75732a8a2c6
Pravin Kori <pravinkori@protonmail.ch>
2025-04-30 00:19:26 +0530

    feat: Add word count analysis to Book Bot
    - Implemented count_words(text) function using str.split() to count the
    number of words.
    - Updated main() to compute and print the total word count from
    'frankenstein.txt' instead of printing the full text.
    - Output message follows the format: "{num_words} words found in the
    document".

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ def get_book_test(file_path):
     
     return file_contents
 
+def count_words(text):
+    words = text.split()
+    return len(words)
+
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_test(book_path)
 
-    print(book_text)
+    num_words = count_words(book_text)
+
+    print(f"{num_words} words found in the document")
 
 main()
