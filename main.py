6a63872112b85cce3c160a23dbe4b1b16efbfbcc
Pravin Kori <pravinkori@protonmail.ch>
2025-05-01 19:48:45 +0530

    feat: Add command-line support for book path
    - Removed hardcoded book path and replaced it with dynamic path from
    sys.argv
    - Added usage message and exit condition if the path argument is missing
    - Improved flexibility of Book Bot for analyzing any text file via
    command-line

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_char_count, sort_char_count
 
 def get_book_test(file_path):
@@ -7,7 +8,11 @@ def get_book_test(file_path):
     return file_contents
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
     book_text = get_book_test(book_path)
 
     num_words = get_num_words(book_text)
