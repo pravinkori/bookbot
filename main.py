e70aa7e37ba61349362e680cb77144103f30719e
Pravin Kori <pravinkori@protonmail.ch>
2025-05-01 19:31:35 +0530

    feat: Print final formatted report with sorted character frequencies
    - Added sort_char_counts(char_dict) to stats.py that filters out
    non-alphabetical characters and sorts by frequency.
    - Updated main.py to use sorted character frequency list and print a
    clean, structured report.
    - Report includes word count and character frequencies, mimicking a
    traditional analysis output.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_char_count
+from stats import get_num_words, get_char_count, sort_char_count
 
 def get_book_test(file_path):
     with open(file_path) as file:
@@ -12,9 +12,21 @@ def main():
 
     num_words = get_num_words(book_text)
     char_counts = get_char_count(book_text)
+    sorted_chars = sort_char_count(char_counts)
 
-    print(f"{num_words} words found in the document")
-    print("\nCharacter frequencies:")
-    print(char_counts)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+    # print(f"{num_words} words found in the document")
+    # print("\nCharacter frequencies:")
+    # print(char_counts)
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,16 @@ def get_char_count(text):
             char_count[char] = 1
     
     return char_count
+
+def sort_on(item):
+    return item["num"]
+
+def sort_char_count(char_dict):
+    sorted_list = []
+    
+    for char in char_dict:
+        if char.isalpha():
+            sorted_list.append({"char": char, "num": char_dict[char]})
+    
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
