07db5255f495be06c73e1ede34663d93f926a3c7
Pravin Kori <pravinkori@protonmail.ch>
2025-11-28 20:57:56 +0530

    feat: Add character count function
    This function takes the text from the book as a string, and returns the
    number of times each character, (including symbols and spaces), appears
    in the string.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_chars_dict, get_num_words
 
 
 def get_book_text(path):
@@ -14,6 +14,9 @@ def main():
 
     num_words = get_num_words(text)
     print(f"Found {num_words} total words")
+
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
 
 main()
