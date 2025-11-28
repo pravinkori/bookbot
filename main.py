854c0cfe31cfc63448229692de5856ef45515181
Pravin Kori <pravinkori@protonmail.ch>
2025-11-28 21:18:01 +0530

    feat: Analysis report function

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from stats import get_chars_dict, get_num_words
+from stats import (
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+    get_num_words,
+)
 
 
 def get_book_text(path):
@@ -6,6 +10,20 @@ def get_book_text(path):
         file_contents = f.read()
 
         return file_contents
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
 def main():
@@ -17,6 +35,9 @@ def main():
 
     chars_dict = get_chars_dict(text)
     print(chars_dict)
+
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
 main()
