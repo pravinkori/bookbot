122664d299573fd90dfe71c6535bc58c29852a73
Pravin Kori <pravinkori@protonmail.ch>
2025-11-28 21:14:49 +0530

    feat: Add characters sorted list
    This function takes the dictionary of characters and their counts and
    returns a sorted list of dictionaries

--- stats.py
+++ stats.py
@@ -12,3 +12,17 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+
+    for char in num_chars_dict:
+        sorted_list.append({"char": char, "num": num_chars_dict[char]})
+
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
